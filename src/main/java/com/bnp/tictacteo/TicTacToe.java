96baac319d09eaa10992071059c76ff58ca2c76f
Hitesh <hiteshgupta52@gmail.com>
2020-12-14 16:08:40 +0530

    Commented/trial code removed

--- src/main/java/com/bnp/tictacteo/TicTacToe.java
+++ src/main/java/com/bnp/tictacteo/TicTacToe.java
@@ -95,28 +95,4 @@ public class TicTacToe {
 		}
 		System.out.println("----------------------------");
 	}
-
-	/*
-	 * public void playX(int i) { moveXs.add(i); }
-	 * 
-	 * public List<Integer> getX() { List<Integer> result = new
-	 * ArrayList<Integer>(); result.addAll(moveXs); return result; }
-	 * 
-	 * public void playO() { for (int i = 1; i < 8; i++) { if (isLegalMove(i)) {
-	 * moveOs.add(i); break; } } }
-	 * 
-	 * public List<Integer> getO() { List<Integer> result = new
-	 * ArrayList<Integer>(); result.addAll(moveOs); return result; }
-	 * 
-	 * public boolean isFinished() { List<List<Integer>> winningPositions = new
-	 * ArrayList<List<Integer>>(); List<Integer> pos1 = list(new Integer[] { 1,
-	 * 4, 7 }); winningPositions.add(pos1); for (List<Integer> pos :
-	 * winningPositions) { if (haveSameElements(pos, moveXs)) { return true; } }
-	 * return false; }
-	 * 
-	 * private boolean haveSameElements(List<Integer> a, List<Integer> b) {
-	 * return (a.size() == b.size()) && a.containsAll(b); }
-	 * 
-	 * private List<Integer> list(Integer[] arr) { return Arrays.asList(arr); }
-	 */
 }
